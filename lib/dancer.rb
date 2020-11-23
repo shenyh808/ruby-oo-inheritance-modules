bc5c7e95f9c4de6d51c396a728bb887d6194469f
shen huang <shenyh33@gmail.com>
2020-11-23 10:33:12 -1000

    commit tree c1cf440bfad764b3687b6c1ba34c375533bfa71e

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,27 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+class Dancer
+    include Dance
+  
+    attr_accessor :name
+  
+    def initialize(name)
+      @name = name
+    end
+end 
+
+  class Dancer
+    extend MetaDancing
+  end 
+
+# class Dancer
+#     include FancyDance::InstanceMethods
+#     extend FancyDance::ClassMethods
+ 
+#      attr_accessor :name
+ 
+#      def initialize(name)
+#       @name = name
+#     end
+# end 
